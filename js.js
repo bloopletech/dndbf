3c7c8db92620892733b932fdbfe78d4d93123fa1
Brenton "B-Train" Fletcher <i@bloople.net>
2013-02-15 04:14:15 +1100

    Use on instead of live, and bind to new #dungeons element instead of document; Use keydown event in .damage to match window keydown event; Prevent hitting space in a field triggering roll

--- js.js
+++ js.js
@@ -9,14 +9,60 @@ $(function() {
     }, 1000, complete ? complete : $.noop);
   };
 
-  $(".damage").live("change", function(event) {
+  $("#dungeons").on("change", ".damage", function(event) {
     var hp = $(this).prev();
     hp.val(parseInt(hp.val()) - parseInt($(this).val())).pulse();
     $(this).val("");
 
     if(parseInt(hp.val()) < 0) $(this).parent().find(".delete-monster").click();
-  }).live("keyup", function() {
+  }).on("keydown", ".damage", function(event) {
     if(event.keyCode == 32) $(this).change();
+  });
+
+  /*$(".change").live("click", function() {
+    $(this).parent().find(".changer").toggle();
+  })*/
+
+  $("#dungeons").on("keydown", "input, button, select", function(event) {
+    if(event.keyCode == 32) event.stopPropagation();
+  });
+
+  function nextMiniature() {
+    var lastIndex = 0;
+    $(".miniature option:selected").each(function(i, option) {
+      if(option.index > lastIndex) lastIndex = option.index;
+    });
+
+    var lastOption = $("#monster-source .miniature option")[lastIndex + 1];
+    if(typeof lastOption == "undefined") {
+      alert("Too many monsters!");
+      return false;
+    }
+    else {
+      return lastOption.value;
+    }
+  }
+
+  $("#dungeons").on("click", ".copy-monster", function() {
+    $(this).parent().after($(this).parent().clone());
+
+    var miniature = $(this).parent().next().find(".miniature");
+    miniature.val(nextMiniature());
+
+    //$(this).parent().find(".change").click();
+  });
+
+  $("#dungeons").on("click", ".delete-monster", function() {
+    if(confirm("Really delete?")) $(this).parent().remove();
+
+    //$(this).parent().find(".change").click();
+  });
+
+  $("#dungeons").on("click", ".add-monster", function() {
+    $(this).parent().find(".monsters").append($("#monster-source").children().clone());
+
+    var miniature = $(this).parent().find(".monsters").children().last().find(".miniature");
+    miniature.val(nextMiniature());
   });
 
   var currentlyRolling = false;
@@ -44,47 +90,6 @@ $(function() {
     });
   });
 
-  /*$(".change").live("click", function() {
-    $(this).parent().find(".changer").toggle();
-  })*/
-
-  function nextMiniature() {
-    var lastIndex = 0;
-    $(".miniature option:selected").each(function(i, option) {
-      if(option.index > lastIndex) lastIndex = option.index;
-    });
-
-    var lastOption = $("#monster-source .miniature option")[lastIndex + 1];
-    if(typeof lastOption == "undefined") {
-      alert("Too many monsters!");
-      return false;
-    }
-    else {
-      return lastOption.value;
-    }
-  }
-
-  $(".copy-monster").live("click", function() {
-    $(this).parent().after($(this).parent().clone());
-
-    var miniature = $(this).parent().next().find(".miniature");
-    miniature.val(nextMiniature());
-
-    //$(this).parent().find(".change").click();
-  });
-
-  $(".delete-monster").live("click", function() {
-    if(confirm("Really delete?")) $(this).parent().remove();
-
-    //$(this).parent().find(".change").click();
-  })
-
-  $(".add-monster").click(function() {
-    $(this).parent().find(".monsters").append($("#monster-source").children().clone());
-
-    var miniature = $(this).parent().find(".monsters").children().last().find(".miniature");
-    miniature.val(nextMiniature());
-  });
 
   $(window).keydown(function(event) {
     if(event.keyCode == 32) $("#roll").click();
